28e8e0371501e8d16cf13ec17677e9d585ebe007
josehermosillaa <jose.hermosillaa@usach.cl>
2023-09-26 23:25:57 -0300

    se logra descargar archivos, falta comprobacion

--- proyectoDjangov1/core/management/commands/download_excell.py
+++ proyectoDjangov1/core/management/commands/download_excell.py
@@ -10,6 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         #podemos darle fechas que queramos
         today = datetime.now()
+        count = 0
         # today = datetime(2023,7,1)
         for i in range(1, today.month):
             
@@ -30,51 +31,63 @@ class Command(BaseCommand):
             #hasta aqui es lo mismo
             file_path = f'data/lic_{today.year}-{i}.csv'
             #transformando los datos de la columna a lowercase, para mejorar el filtro
-            df = pd.read_csv(file_path, sep = ";", lineterminator='\n', encoding='latin-1')
+            df = pd.read_csv(file_path, sep = ";", encoding='latin-1', dtype='str')
             # df = pd.read_csv('data/lic_2022-12.csv', encoding='latin-1')
             df['Nombre producto genrico'] = df['Nombre producto genrico'].apply(lambda x:x.upper()) 
             #filter = """`Nombre producto genrico` == 'CONSTRUCCIÓN DE CASAS'|`Nombre producto genrico` == 'CONSTRUCCIÓN DE ACERAS, VEREDAS, CUNETAS'| `Nombre producto genrico` == 'CONSTRUCCIÓN DE EDIFICIOS Y DEPARTAMENTOS'|`Nombre producto genrico` == 'CONSTRUCCIÓN DE LOCALES, PLANTAS COMERCIALES O INDUSTRIALES'|`Nombre producto genrico` == 'CONSTRUCCIÓN DE MUROS DE CONTENCIÓN'|`Nombre producto genrico` == 'CONSTRUCCIÓN DE OBRAS CIVILES'"""
+            #seleccionamos las columnas de interes 
+            df2 = df[[
+                'CodigoExterno',
+                'Nombre',
+                'Descripcion',
+                'NombreOrganismo',
+                'sector',
+                'RutUnidad',
+                'ComunaUnidad',
+                'RegionUnidad',
+                'FechaCreacion',
+                'FechaCierre',
+                'FechaAdjudicacion',
+                'FechaEstimadaAdjudicacion',
+                'MontoEstimado',
+                'Rubro1',
+                'Rubro2',
+                'Nombre producto genrico',
+                'RutProveedor',
+                'NombreProveedor',
+                'RazonSocialProveedor',
+                'Monto Estimado Adjudicado',
+                'MontoLineaAdjudica',
+                'Oferta seleccionada']].copy()
+            #tenemos que algunos campos deberian ser numericos
+            if is_object_dtype( df2['MontoLineaAdjudica']):
+                df2['MontoLineaAdjudica'] = df2['MontoLineaAdjudica'].str.replace(',','.')
+            #if is_object_dtype( df2['CantidadAdjudicada']):
+            #   df2['CantidadAdjudicada'] = df2['CantidadAdjudicada'].str.replace(',','.')
+            if is_object_dtype( df2['MontoEstimado']):
+                df2['MontoEstimado'] = df2['MontoEstimado'].str.replace(',','.')
+            if is_object_dtype( df2['Monto Estimado Adjudicado']):
+                df2['Monto Estimado Adjudicado'] = df2['Monto Estimado Adjudicado'].str.replace(',','.')
             
-            if is_object_dtype( df['MontoLineaAdjudica']):
-                df['MontoLineaAdjudica'] = df['MontoLineaAdjudica'].str.replace(',','.')
-            if is_object_dtype( df['CantidadAdjudicada']):
-                df['CantidadAdjudicada'] = df['CantidadAdjudicada'].str.replace(',','.')
-            suma_monto_linea = []
-            cantidad_monto_linea = []
-            suma_cantidad_adjudica = []
-            for nombre in nombre_producto_generico:
-                # print(nombre)
-                df2 = df.query(f"`Nombre producto genrico` == '{nombre}'")[['Nombre producto genrico','MontoLineaAdjudica','CantidadAdjudicada']]
-                
-                #convertimos los datos str a float
-                # df['MontoLineaAdjudica'] = df['MontoLineaAdjudica'].str.replace(',','.')
-                # df['CantidadAdjudicada'] = df['CantidadAdjudicada'].str.replace(',','.')
-                df2['MontoLineaAdjudica'] = df2['MontoLineaAdjudica'].astype(float)
-                df2['CantidadAdjudicada'] = df2['CantidadAdjudicada'].astype(float)
-                
-                suma_monto_linea.append(df2['MontoLineaAdjudica'].sum())
-                suma_cantidad_adjudica.append(df2['CantidadAdjudicada'].sum())
-                cantidad_monto_linea.append(df2.query("`MontoLineaAdjudica`>0")['MontoLineaAdjudica'].count())
-                # print(type(suma_monto_linea[0]))
-                # print(type(suma_cantidad_adjudica[0]))
-                try:
-                    Cementera.objects.create(
-                        nombre=nombre, 
-                        linea=suma_monto_linea[0].item(), 
-                        cantidad=suma_cantidad_adjudica[0].item(), 
-                        cuenta=int(cantidad_monto_linea[0].item()),
-                        codigo_fecha=f'{today.month}-{today.year}',
-                        monto_linea=suma_monto_linea[0].item(),
-                        )
-                    suma_monto_linea = []
-                    cantidad_monto_linea = []
-                    suma_cantidad_adjudica = []
-                except:
-                    self.stdout.write(self.style.ERROR('Datos ya cargados'))
-            success = True
+            df2['MontoLineaAdjudica'] = df2['MontoLineaAdjudica'].astype(float)
+            #df2['CantidadAdjudicada'] = df2['CantidadAdjudicada'].astype(float)
+            df2['MontoEstimado'] = df2['MontoEstimado'].astype(float)
+            df2['Monto Estimado Adjudicado'] = df2['Monto Estimado Adjudicado'].astype(float)  
+
+            #filtramos los datos de MontoLineaAdjudica >100.000.000 (precio de una casa)
+            def filter_by_monto(row):
+                return row['MontoLineaAdjudica'] > 100000000
+            
+            filtered_df2 = df2[df2.apply(filter_by_monto, axis=1)]
+            #filtramos por nombres genericos
+            filter_nombres = df['Nombre producto genrico'].isin(nombre_producto_generico)
+            excell_inicial = filtered_df2[filter_nombres]
+            
+            # Export the DataFrame to an Excel file
+            excell_inicial.to_excel(f'./download/{i}-{today.year}.xlsx', index=False)
             self.stdout.write(
                 self.style.SUCCESS(
-                        "Se han cargado los datos correctamente"
+                        f"Se ha creado exitosamente'{i}-{today.year}.xlsx' "
                     )
                 )
             os.remove(file_path)
